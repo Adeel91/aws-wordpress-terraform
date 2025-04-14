2b58daf23e1284c03553751bdbdc12dc7c1bdac3
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2025-04-14 14:59:46 +0200

    created vpc and public and private subnets using modular structure

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,1 +1,10 @@
 # Creating Subnet
+resource "aws_subnet" "this" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.subnet_cidr
+  availability_zone       = var.availability_zone
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,1 +1,22 @@
 # Create variables for Subnets
+variable "vpc_id" {
+  description = "ID of the VPC to associate with the subnet"
+  type        = string
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for the subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the subnet"
+  type        = string
+  default     = "us-west-2a"
+}
+
+variable "subnet_name" {
+  description = "Name tag for the subnet"
+  type        = string
+  default     = "public-subnet"
+}
