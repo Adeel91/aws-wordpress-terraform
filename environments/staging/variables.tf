224d8bdb24d07d54be1b06ce1ee82af5cc1cc0cc
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2025-04-16 16:25:00 +0200

    testing vpc only

--- environments/staging/variables.tf
+++ environments/staging/variables.tf
@@ -4,28 +4,28 @@ variable "vpc_name" {
   default     = ""
 }
 
-# Public Subnet Settings
-variable "public_subnet_name" {
-  description = "Name tag for the subnet"
-  type        = string
-  default     = ""
-}
+# # Public Subnet Settings
+# variable "public_subnet_name" {
+#   description = "Name tag for the subnet"
+#   type        = string
+#   default     = ""
+# }
 
-variable "public_cidr_block" {
-  description = "Public CIDR block"
-  type        = string
-  default     = ""
-}
+# variable "public_cidr_block" {
+#   description = "Public CIDR block"
+#   type        = string
+#   default     = ""
+# }
 
-# Private Subnet Settings
-variable "private_subnet_name" {
-  description = "Defaut VPC name"
-  type        = string
-  default     = ""
-}
+# # Private Subnet Settings
+# variable "private_subnet_name" {
+#   description = "Defaut VPC name"
+#   type        = string
+#   default     = ""
+# }
 
-variable "private_cidr_block" {
-  description = "Private CIDR block"
-  type        = string
-  default     = ""
-}
+# variable "private_cidr_block" {
+#   description = "Private CIDR block"
+#   type        = string
+#   default     = ""
+# }
--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -7,7 +7,7 @@ variable "vpc_id" {
 variable "subnet_cidr" {
   description = "CIDR block for the subnet"
   type        = string
-  default   = "10.0.1.0/24"
+  default     = "10.0.1.0/24"
 }
 
 variable "availability_zone" {
