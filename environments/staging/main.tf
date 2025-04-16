68bc91d2ba0cfa07864fb50941a2ce216fc775ea
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2025-04-16 16:26:14 +0200

    pushed latest changes to test vpc only

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -4,20 +4,20 @@ module "vpc" {
   vpc_name          = var.vpc_name
 }
 
-# Create a Public Subnet
-module "public_subnet" {
-  source            = "../../modules/subnet"
-  vpc_id            = module.vpc.vpc_id
-  subnet_cidr       = var.public_cidr_block
-  subnet_name       = var.public_subnet_name
-  depends_on        = [ module.vpc ]
-}
+# # Create a Public Subnet
+# module "public_subnet" {
+#   source            = "../../modules/subnet"
+#   vpc_id            = module.vpc.vpc_id
+#   subnet_cidr       = var.public_cidr_block
+#   subnet_name       = var.public_subnet_name
+#   depends_on        = [ module.vpc ]
+# }
 
-# Create a Private Subnet
-module "private_subnet" {
-  source            = "../../modules/subnet"
-  vpc_id            = module.vpc.vpc_id
-  subnet_cidr       = var.private_cidr_block
-  subnet_name       = var.private_subnet_name
-  depends_on        = [ module.vpc ]
-}
+# # Create a Private Subnet
+# module "private_subnet" {
+#   source            = "../../modules/subnet"
+#   vpc_id            = module.vpc.vpc_id
+#   subnet_cidr       = var.private_cidr_block
+#   subnet_name       = var.private_subnet_name
+#   depends_on        = [ module.vpc ]
+# }
