2d1787860f5ced50701c223505633386a91202b0
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2025-04-16 16:43:56 +0200

    added hardcoded values for subnet public and private

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -4,20 +4,20 @@ module "vpc" {
   vpc_name          = "wordpress-vpc" # var.staging_vpc_name
 }
 
-# # Create a Public Subnet
-# module "public_subnet" {
-#   source            = "../../modules/subnet"
-#   vpc_id            = module.vpc.vpc_id
-#   subnet_cidr       = var.public_cidr_block
-#   subnet_name       = var.public_subnet_name
-#   depends_on        = [ module.vpc ]
-# }
+# Create a Public Subnet
+module "public_subnet" {
+  source            = "../../modules/subnet"
+  vpc_id            = module.vpc.vpc_id
+  subnet_cidr       = "10.0.1.0/24" # var.public_cidr_block
+  subnet_name       = "public-subnet" # var.public_subnet_name
+  depends_on        = [ module.vpc ]
+}
 
-# # Create a Private Subnet
-# module "private_subnet" {
-#   source            = "../../modules/subnet"
-#   vpc_id            = module.vpc.vpc_id
-#   subnet_cidr       = var.private_cidr_block
-#   subnet_name       = var.private_subnet_name
-#   depends_on        = [ module.vpc ]
-# }
+# Create a Private Subnet
+module "private_subnet" {
+  source            = "../../modules/subnet"
+  vpc_id            = module.vpc.vpc_id
+  subnet_cidr       = "10.0.2.0/24" # var.private_cidr_block
+  subnet_name       = "private-subnet" # var.private_subnet_name
+  depends_on        = [ module.vpc ]
+}
