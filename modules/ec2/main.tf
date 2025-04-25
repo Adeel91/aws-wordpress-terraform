18c789e229de8ef37ac5368403deab8ebb1255e7
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2025-04-25 13:35:26 +0200

    retrieved security group id from module sg

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "bastion" {
   ami                         = "ami-0440d3b780d96b29d"
   instance_type               = "t2.micro"
   subnet_id                   = var.public_subnet_cidr_az1
-  vpc_security_group_ids      = [aws_security_group.public_sg.id]
+  vpc_security_group_ids      = [module.public_sg.public_sg_id]
   associate_public_ip_address = true
   key_name                    = var.key_name
 
@@ -15,7 +15,7 @@ resource "aws_instance" "wordpress_az1" {
   ami                    = "ami-0440d3b780d96b29d"
   instance_type          = "t2.micro"
   subnet_id              = var.private_subnet_cidr_az1
-  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  vpc_security_group_ids = [module.private_sg.private_sg_id]
   key_name               = var.key_name
   associate_public_ip_address = false
 
@@ -34,7 +34,7 @@ resource "aws_instance" "wordpress_az2" {
   ami                    = "ami-0440d3b780d96b29d"
   instance_type          = "t2.micro"
   subnet_id              = var.private_subnet_cidr_az2
-  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  vpc_security_group_ids = [module.private_sg.private_sg_id]
   key_name               = var.key_name
   associate_public_ip_address = false
 
