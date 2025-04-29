c3c59289ca68944f57a9af881c4b885713022391
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2025-04-29 08:44:34 +0200

    fix: test commented-out code to check security groups creation flow

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -106,51 +106,51 @@ module "private_rds_sg" {
   ]
 }
 
-# Create Bastion Host in 1 of the public subnet
-module "ec2_bastion_host" {
-  source                 = "../../modules/ec2"
-  public_subnet_cidr_az1 = module.public_subnet.subnets["${var.project_name}-public-subnet1"].cidr # this is the first public subnet in the list of AZ1 public subnet
-  project_name           = var.project_name
-  security_group_id      = module.public_sg.private_sg_id
-  depends_on             = [module.private_sg]
-}
+# # Create Bastion Host in 1 of the public subnet
+# module "ec2_bastion_host" {
+#   source                 = "../../modules/ec2"
+#   public_subnet_cidr_az1 = module.public_subnet.subnets["${var.project_name}-public-subnet1"].cidr # this is the first public subnet in the list of AZ1 public subnet
+#   project_name           = var.project_name
+#   security_group_id      = module.public_sg.private_sg_id
+#   depends_on             = [module.private_sg]
+# }
 
-# Create WordPress Instance in 1 of the private subnet
-module "ec2_wordpress_az1" {
-  source                  = "../../modules/ec2"
-  private_subnet_cidr_az1 = module.private_subnet.subnets["${var.project_name}-private-subnet1"].cidr # this is the first private subnet in the list of AZ1 private subnet
-  project_name            = var.project_name
-  security_group_id       = module.private_sg.private_sg_id
-  depends_on              = [module.private_sg]
-}
+# # Create WordPress Instance in 1 of the private subnet
+# module "ec2_wordpress_az1" {
+#   source                  = "../../modules/ec2"
+#   private_subnet_cidr_az1 = module.private_subnet.subnets["${var.project_name}-private-subnet1"].cidr # this is the first private subnet in the list of AZ1 private subnet
+#   project_name            = var.project_name
+#   security_group_id       = module.private_sg.private_sg_id
+#   depends_on              = [module.private_sg]
+# }
 
-# Create WordPress Instance in second of the private subnet
-module "ec2_wordpress_az2" {
-  source                  = "../../modules/ec2"
-  private_subnet_cidr_az2 = module.private_subnet.subnets["${var.project_name}-private-subnet2"].cidr # this is the second private subnet in the list of AZ2 private subnet
-  project_name            = var.project_name
-  security_group_id       = module.private_sg.private_sg_id
-  depends_on              = [module.private_sg]
-}
+# # Create WordPress Instance in second of the private subnet
+# module "ec2_wordpress_az2" {
+#   source                  = "../../modules/ec2"
+#   private_subnet_cidr_az2 = module.private_subnet.subnets["${var.project_name}-private-subnet2"].cidr # this is the second private subnet in the list of AZ2 private subnet
+#   project_name            = var.project_name
+#   security_group_id       = module.private_sg.private_sg_id
+#   depends_on              = [module.private_sg]
+# }
 
-# Reference the RDS MariaDB module and pass necessary parameters
-module "rds" {
-  source                = "../../modules/rds"
-  project_name          = var.project_name
-  db_name               = "aws-wordpress-terraform"
-  db_username           = "root"
-  db_password           = "root"
-  private_subnet_ids    = [module.private_subnet.subnets["${var.project_name}-private-subnet1"].cidr, module.private_subnet.subnets["${var.project_name}-private-subnet2"].cidr]
-  security_group_id     = module.private_rds_sg.private_rds_sg_id
-  db_subnet_group_name  = "${var.project_name}-mariadb-subnet-group"
-}
+# # Reference the RDS MariaDB module and pass necessary parameters
+# module "rds" {
+#   source                = "../../modules/rds"
+#   project_name          = var.project_name
+#   db_name               = "aws-wordpress-terraform"
+#   db_username           = "root"
+#   db_password           = "root"
+#   private_subnet_ids    = [module.private_subnet.subnets["${var.project_name}-private-subnet1"].cidr, module.private_subnet.subnets["${var.project_name}-private-subnet2"].cidr]
+#   security_group_id     = module.private_rds_sg.private_rds_sg_id
+#   db_subnet_group_name  = "${var.project_name}-mariadb-subnet-group"
+# }
 
-module "alb" {
-  source            = "../../modules/alb"
-  project_name      = var.project_name
-  security_group_id = module.public_lg_sg.public_lb_sg_id
-  subnet_ids        = [module.public_subnet.subnets["${var.project_name}-public-subnet1"].cidr, module.public_subnet.subnets["${var.project_name}-public-subnet2"].cidr] # Use the public subnets in AZ1 and AZ2
-  vpc_id            = module.vpc.vpc_id
-  wordpress_az1_id  = module.ec2_wordpress_az1.ec2_wordpress_az1_id
-  wordpress_az2_id  = module.ec2_wordpress_az2.ec2_wordpress_az2_id
-}
+# module "alb" {
+#   source            = "../../modules/alb"
+#   project_name      = var.project_name
+#   security_group_id = module.public_lg_sg.public_lb_sg_id
+#   subnet_ids        = [module.public_subnet.subnets["${var.project_name}-public-subnet1"].cidr, module.public_subnet.subnets["${var.project_name}-public-subnet2"].cidr] # Use the public subnets in AZ1 and AZ2
+#   vpc_id            = module.vpc.vpc_id
+#   wordpress_az1_id  = module.ec2_wordpress_az1.ec2_wordpress_az1_id
+#   wordpress_az2_id  = module.ec2_wordpress_az2.ec2_wordpress_az2_id
+# }
