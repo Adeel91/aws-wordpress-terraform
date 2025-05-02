b218c1097c24665655820efc1ac1a103f5f0ee5e
Muhammad Adeel <muhammad_adeel91@yahoo.com>
2025-05-02 12:36:14 +0200

    fix: disabled object locking

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "static_website" {
-  bucket = var.bucket_name
+  bucket              = var.bucket_name
+  object_lock_enabled = false
 
   tags = {
     Name = "${var.project_name}-bucket"
